400595628ee61e5618482e5a0242d5578eeb1db9
Samuel Pinard <samuel.pinard.1@ens.etsmtl.ca>
2021-03-03 14:01:44 -0500

    Réseau de neuronne et entrainement Done

---
+++ SaveGeneratedImage2.m
@@ -0,0 +1,16 @@
+function [] = SaveGeneratedImage2(GeneratedImage,SaveFile,ProgramFile)
+%SAVEIMAGE Summary of this function goes here
+%Sauvegarde les images dans le dossier "SaveFile"
+%Prend en argument la matrice des images, le répertoire "SaveFile" et le
+%répertoire ProgramFile où le code est sauvegarder
+%   Detailed explanation goes here
+
+cd(SaveFile)
+for k = 1:numel(GeneratedImage)   
+  fileName = sprintf('image_%04d.jpg', i+k);     
+  imwrite( GeneratedImage{k}, fileName);  
+end
+
+cd(ProgramFile)
+end
+
